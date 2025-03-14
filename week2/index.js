feebdf1bbfe9d4268ca91f47f2ed4763b22faf76
Eileen-Qian <yiqianc.14@gmail.com>
2025-03-14 15:03:55 +0800

    feat: Update week2

--- week2/index.js
+++ week2/index.js
@@ -3,37 +3,37 @@
 // 若不確定答案，可將 code 貼在 chrome console 顯示後，再回頭補知識點。
 let a = 9;
 let b = 13;
-console.log(a > 0);
-console.log(b > a);
-console.log(a + b > 1);
+console.log(a > 0); // true
+console.log(b > a); // true
+console.log(a + b > 1); // true
 let c = 51;
 let d = 163;
-console.log(c == d);
-console.log(c !== d);
+console.log(c == d); // false
+console.log(c !== d); // true
 let e = 28;
 let f = 45;
-console.log(f >= e);
-console.log(f != e);
-console.log(f == e);
+console.log(f >= e); // true
+console.log(f != e); // true
+console.log(f == e); // false
 
 // ### 題目二：比較運算子 + 強制轉型
 // 請回答每個 console.log 的值為？
 let g = 8;
 let h = '8';
-console.log(g * h == 88);
-console.log(g * h == 64);
-console.log(g * h === 64);
+console.log(g * h == 88); // false
+console.log(g * h == 64); // true
+console.log(g * h === 64); // true
 
 let i = '9';
 let j = '9';
-console.log(i + j == 99);
-console.log(i + j === '99');
-console.log(i + j === 99);
+console.log(i + j == 99); // true
+console.log(i + j === '99'); // true
+console.log(i + j === 99); // false
 
 var k = 3;
 var l = '8';
 // 請文字解釋為什麼
-console.log(k * l > 21);
+console.log(k * l > 21); // true，相乘時 javascript 會自動轉型把l轉為 8，3 * 8 為 24，24 > 21 為 true
 
 // ### 題目三：邏輯運算子 + if, else
 // 情境：健身房週年慶，買課程送贈品
@@ -49,11 +49,12 @@ let BobPrice = 1800; /* Bob 消費金額 */
 let BobIsVip = false; /* Bob 是否為 VIP */
 
 // 練習：（可自行將下方程式碼的註解刪除，完成答題）
-// if () {
-//   console.Log（"客戶您好，您有符合贈品資格"）；
-// } else {
-//   console.Log（"客戶您好，您沒有符合贈品資格"）
-// }
+if (BobPrice >= giftPriceRule || BobIsVip) {
+  console.log("客戶您好，您有符合贈品資格")
+  giftNum -= 1
+} else {
+  console.log("客戶您好，您沒有符合贈品資格")
+}
 
 console.log(`贈品還剩下${giftNum}個`);
 
@@ -72,7 +73,13 @@ let baseBonus = 6000; // 基本獎金
 let coachBonus = baseBonus; // 教練業績獎金帳單，並已加入條件一基本獎金
 
 // 練習：計算教練業績獎金
-
+if(coachIncome <= 100000) {
+  coachBonus = baseBonus + coachIncome * 0.1
+} else if (coachIncome > 100000 && coachIncome <= 300000) {
+  coachBonus = baseBonus + coachIncome * 0.15
+} else {
+  coachBonus = baseBonus + coachIncome * 0.2
+}
 console.log(`小明總共需支付 $${coachBonus} 獎金`);
 
 // ### 題目五：剪刀石頭布
@@ -86,6 +93,15 @@ if (playerA === '剪刀' && playerB === '剪刀') {
   console.log('平手');
 }
 // 練習：使用 if, else if, else 判斷輸贏
+if (playerA === playerB){
+  console.log('平手');
+} else if ((playerA === '剪刀' && playerB === '布') || (playerA === '石頭' && playerB === '剪刀') || (playerA === '布' && playerB === '石頭')){
+  console.log("玩家A獲勝");
+} else if((playerA === '剪刀' && playerB === '石頭') || (playerA === '石頭' && playerB === '布') || (playerA === '布' && playerB === '剪刀')){
+  console.log("玩家B獲勝");
+} else {
+  console.log("有人亂出");
+}
 
 // ### 題目六：陣列、物件變數定義
 // 這是一間位於高雄市的健身房，名為「高雄市健身教練聯盟」，專注於提供高品質的健身指導服務。健身房內有多位專業教練，以下是兩位教練的詳細介紹：
@@ -107,7 +123,55 @@ if (playerA === '剪刀' && playerB === '剪刀') {
 - 是否接收新學員：否
 */
 
-const gymCoach = {}; // 練習：使用物件變數定義兩位教練的資訊
+// 練習：使用物件變數定義兩位教練的資訊
+const gymCoach = {
+  coachWang: {
+    expertise: ['力量訓練', '減重課程'],
+    courses: [
+      {
+        name: '個人訓練課程',
+        price: 2000,
+        time: 60,
+        avalible: true
+      },
+      {
+        name: '團體訓練課程',
+        price: 1500,
+        time: 90,
+        avalible: false
+      },
+    ],
+    profile: {
+      title: '',
+      experiences: 5,
+      description: '專精於提升學員的肌力與減脂，適合希望快速達成體能目標的學員。'
+    },
+    newStudentsAllowed: false
+  },
+  coachLee: {
+    expertise: ['瑜伽', '體態雕塑'],
+    courses: [
+      {
+        name: '個人訓練課程',
+        price: 1800,
+        time: 50,
+        avalible: false
+      },
+      {
+        name: '團體訓練課程',
+        price: 1200,
+        time: 70,
+        avalible: true
+      },
+    ],
+    profile: {
+      title: '瑜伽大師',
+      experiences: 10,
+      description: '擅長幫助學員雕塑完美體態，適合希望改善姿態與柔軟度的學員'
+    },
+    newStudentsAllowed: false
+  }
+};
 
 console.log(gymCoach);
 
